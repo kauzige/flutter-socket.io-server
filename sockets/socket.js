ae6308b5f80f64e1595c756dda16dcb08d99058b
Dani <danielroldanys@gmail.com>
2022-03-21 14:04:52 +0100

    Cambios sobre bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,22 @@
 const { io } = require("../index");
+const Band = require("../models/band");
+const Bands = require("../models/bands");
+
+const bands = new Bands();
+
+bands.addBand(new Band("AC/DC"));
+bands.addBand(new Band("Bon Jovi"));
+bands.addBand(new Band("Aerosmith"));
+bands.addBand(new Band("Green Day"));
+
+console.log(bands);
 
 // * Mensajes de Sockets
 /// [client] : una computadora o dispositivo que se conecto a mi socket server
 io.on("connection", (client) => {
   console.log("Cliente conectado");
+
+  client.emit("active-bands", bands.getBands());
 
   client.on("disconnect", () => {
     /* Cuando se desconecte */
@@ -14,5 +27,28 @@ io.on("connection", (client) => {
     console.log("mensaje!!!", payload);
 
     io.emit("mensaje", { admin: "Nuevo mensaje " });
+  });
+
+  // client.on("emitir-mensaje", (payload) => {
+  //   // console.log(payload);
+  //   // io.emit("nuevo-mensaje", payload); // emite a todos !
+  //   client.broadcast.emit("nuevo-mensaje", payload); // emite a todos menos al que lo emitio
+  // });
+
+  client.on("vote-band", (payload) => {
+    bands.voteBand(payload.id);
+    io.emit("active-bands", bands.getBands());
+  });
+
+  // Escuchar add-band
+  client.on("add-band", (payload) => {
+    bands.addBand(new Band(payload.name));
+    io.emit("active-bands", bands.getBands());
+  });
+
+  // Borrar
+  client.on("delete-band", (payload) => {
+    bands.deleteBand(payload.id);
+    io.emit("active-bands", bands.getBands());
   });
 });
